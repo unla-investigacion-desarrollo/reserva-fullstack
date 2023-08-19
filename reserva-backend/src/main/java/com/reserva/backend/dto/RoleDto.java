8e6386c70780a3197c1372976ff649e770a43c8d
Franco Ponce <francoponce20@hotmail.com>
2023-08-18 22:44:59 -0300

    fix role null

--- reserva-backend/src/main/java/com/reserva/backend/dto/RoleDto.java
+++ reserva-backend/src/main/java/com/reserva/backend/dto/RoleDto.java
@@ -1,5 +1,34 @@
 package com.reserva.backend.dto;
 
 public class RoleDto {
+	
+	private long id;
+	private String name;
+	
+	public RoleDto(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public RoleDto() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
