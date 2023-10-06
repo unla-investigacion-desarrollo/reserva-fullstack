397f3c4552cf146e7053bd633bf2320e422360ad
Franco Ponce <francoponce20@hotmail.com>
2023-10-06 20:31:45 -0300

    se agregó la documentación parcial de la entidad avistamiento en swagger

--- reserva-backend/src/main/java/com/reserva/backend/controllers/SightingController.java
+++ reserva-backend/src/main/java/com/reserva/backend/controllers/SightingController.java
@@ -18,6 +18,12 @@ import com.reserva.backend.dto.SightingRequestDto;
 import com.reserva.backend.dto.UpdateStatusDto;
 import com.reserva.backend.services.ISightingService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
 @RequestMapping("/sighting")
 public class SightingController {
@@ -25,21 +31,41 @@ public class SightingController {
     @Autowired
     private ISightingService sightingService;
 
+    @Operation(summary = "realiza la creacion completa de un avistamiento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "created", content = @Content),
+            @ApiResponse(responseCode = "400", description = "el Tipo_Avistamiento no es valido", content = @Content),
+            @ApiResponse(responseCode = "404", description = "el usuario no fue encontrado", content = @Content)
+        })
     @PostMapping("/create")
     public ResponseEntity<?> create(@Valid @RequestBody SightingRequestDto request){
         return new ResponseEntity<>(sightingService.create(request), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "trae el avistamiento dado su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ok", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "no hay ningun avistamiento con ese id", content = @Content(mediaType = "application/json"))
+        })
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable(name = "id") long id){
         return ResponseEntity.ok(sightingService.getById(id));
     }
 
+    @Operation(summary = "trae el avistamiento completo por idUsuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ok", content = @Content(mediaType = "application/json")) 
+    })
     @GetMapping("/getByUser/{userId}")
     public ResponseEntity<?> getByUserId(@PathVariable(name = "userId") long userId){
         return ResponseEntity.ok(sightingService.getByUserId(userId));
     }
 
+    @Operation(summary = "trae todos los avistamientos con paginacion, filtros de estado y tipo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ok", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "no se pudieron listar los avistamientos", content = @Content(mediaType = "application/json")),
+    })
     @GetMapping
     public ResponseEntity<?> getAll(@RequestParam(value = "status", defaultValue = "") String status,
 			@RequestParam(value = "type", defaultValue = "") String type,
@@ -50,8 +76,13 @@ public class SightingController {
         return ResponseEntity.ok(sightingService.getAll(status, type, page, size, orderBy, sortBy));
     }
 
-    @PreAuthorize("hasRole('ROLE_PERSONAL_RESERVA')")
+    @Operation(summary = "realiza la actualizacion de el estado de los avistamientos", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ok", content = @Content),
+            @ApiResponse(responseCode = "404", description = "usuario o avistamiento no enecontrado", content = @Content)
+    })
     @PostMapping("/status")
+    @PreAuthorize("hasRole('ROLE_PERSONAL_RESERVA')")
     public ResponseEntity<?> updateStatus(@RequestBody UpdateStatusDto request){
         return ResponseEntity.ok(sightingService.updateStatus(request));
     }
