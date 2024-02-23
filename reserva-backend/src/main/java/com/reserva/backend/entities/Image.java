dcddd17b6ae13f8981201e546d004c714bf5d162
Franco Ponce <francoponce20@hotmail.com>
2024-02-23 18:30:52 -0300

    entidad para alamacenar las url's

---
+++ reserva-backend/src/main/java/com/reserva/backend/entities/Image.java
@@ -0,0 +1,32 @@
+package com.reserva.backend.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "sighting_id")
+    private Sighting sighting;
+    
+}
